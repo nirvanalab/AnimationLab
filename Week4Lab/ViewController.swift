6b8a0fe4aa38ec49164b888812c2b4bc0b9b46f3
Vidhur Voora <lab.nirvana@gmail.com>
2017-10-03 22:32:50 -0700

    Added Milestone 3 functionality

--- Week4Lab/ViewController.swift
+++ Week4Lab/ViewController.swift
@@ -8,34 +8,82 @@
 
 import UIKit
 
+enum TrayState {
+    case Open
+    case Closed
+}
+
 class ViewController: UIViewController {
 
     @IBOutlet weak var trayView: UIView!
     
+    var trayOpenCenter:CGPoint = CGPoint.zero
+    var trayCloseCenter:CGPoint = CGPoint.zero
+    
+    var currentTrayState:TrayState = .Open {
+        didSet {
+            moveTray(position: currentTrayState)
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        //initialize the tray open center
+        trayOpenCenter = CGPoint(x: trayView.frame.midX, y: trayView.frame.midY)
+        //initialize the tray close center
+        trayCloseCenter = CGPoint(x: trayView.frame.midX, y: trayView.frame.maxY + 75)
+        
     }
 
 
     @IBAction func onTrayPan(_ sender: UIPanGestureRecognizer) {
         
-        let translation = sender.translation(in: sender.view)
-        
         switch sender.state {
             case .began:
                 break
             case .changed:
+                let translation = sender.translation(in: sender.view)
                 trayView.center = CGPoint(x: trayView.center.x + translation.x, y: trayView.center.y+translation.y)
                 break
             case .ended:
+                let velocity = sender.velocity(in: sender.view)
+                print (velocity.y)
+                currentTrayState = (velocity.y <= 0) ? TrayState.Open : TrayState.Closed
+                
                 break
             default:
                 break
         }
         sender.setTranslation(CGPoint.zero, in: self.view)
     }
-
+    
+    
+    @IBAction func onTapGesture(_ sender: UITapGestureRecognizer) {
+        
+        currentTrayState = currentTrayState == .Open ? .Closed : .Open
+    }
+    
+    func moveTray(position:TrayState) {
+        var trayPosition = CGPoint.zero
+        switch position {
+        case .Open:
+            trayPosition = trayOpenCenter
+            break
+        case .Closed:
+            trayPosition = trayCloseCenter
+            break
+            
+        }
+        
+        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping:
+            0.3, initialSpringVelocity: 1.0, options: [], animations: {
+                
+                self.trayView.center = trayPosition
+                
+        }) { _ in
+            
+        }
+    }
 
 }
 
