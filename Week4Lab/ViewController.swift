fd6a6dcd9487ea7a9de2e80a1703164338120ce4
Vidhur Voora <lab.nirvana@gmail.com>
2017-10-04 21:14:40 -0700

    Misc fixes

--- Week4Lab/ViewController.swift
+++ Week4Lab/ViewController.swift
@@ -17,15 +17,20 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var trayView: UIView!
     
+    @IBOutlet weak var trayArrow: UIImageView!
+    
     var trayOpenCenter:CGPoint = CGPoint.zero
     var trayCloseCenter:CGPoint = CGPoint.zero
     var newlyCreatedFace: UIImageView!
     
     var currentTrayState:TrayState = .Open {
         didSet {
-            moveTray(position: currentTrayState)
+            moveTray(currentState: currentTrayState)
         }
     }
+    
+    var previousState:TrayState = .Open
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -35,7 +40,6 @@ class ViewController: UIViewController {
         trayCloseCenter = CGPoint(x: trayView.frame.midX, y: trayView.frame.maxY + 75)
         
     }
-
 
     @IBAction func onTrayPan(_ sender: UIPanGestureRecognizer) {
         
@@ -51,7 +55,6 @@ class ViewController: UIViewController {
                 break
             case .ended:
                 let velocity = sender.velocity(in: sender.view)
-                print (velocity.y)
                 currentTrayState = (velocity.y <= 0) ? TrayState.Open : TrayState.Closed
                 
                 break
@@ -77,8 +80,8 @@ class ViewController: UIViewController {
             
             // Create a new image view that has the same image as the one currently panning
             newlyCreatedFace = UIImageView.init(frame: (sender.view?.frame)!)
+            newlyCreatedFace.isUserInteractionEnabled = true
             newlyCreatedFace.image = imageView.image
-            
             
             // Add the new face to the tray's parent view.
             view.addSubview(newlyCreatedFace)
@@ -90,6 +93,35 @@ class ViewController: UIViewController {
             // main view, you have to offset the coordinates
             newlyCreatedFace.center.y += trayView.frame.origin.y
             
+            //add pan gesture to newly created face
+            let smileyPan = UIPanGestureRecognizer(target: self, action: #selector(onIndividualEmoticonPan(_:)))
+            smileyPan.delegate = self
+            newlyCreatedFace.addGestureRecognizer(smileyPan)
+            
+            //add pinch gesture to newly created face
+            let smileyPinch = UIPinchGestureRecognizer(target: self, action: #selector(onIndividualEmoticonPinch(_:)))
+            smileyPinch.delegate = self
+            newlyCreatedFace.addGestureRecognizer(smileyPinch)
+            
+            let smileyDoubleTap = UITapGestureRecognizer(target: self, action: #selector(onEmoticonTap(_:)))
+            smileyDoubleTap.numberOfTapsRequired = 2
+            newlyCreatedFace.addGestureRecognizer(smileyDoubleTap)
+            
+            //add rotate gesture to newly created face
+            let smileyRotate = UIRotationGestureRecognizer(target: self, action: #selector(onIndividualEmoticonRotate(_:)))
+            smileyRotate.delegate = self
+            newlyCreatedFace.addGestureRecognizer(smileyRotate)
+            
+            
+            
+            //scale the newlyCreatedFace to 1.5x
+            UIView.animate(withDuration: 0.5, animations: {
+                let scaleTransform = CGAffineTransform.init(scaleX: 1.5, y: 1.5)
+                self.newlyCreatedFace.transform = scaleTransform
+            })
+            
+            
+            
             break
         case .changed:
             if let view = sender.view {
@@ -98,6 +130,11 @@ class ViewController: UIViewController {
             }
             break
         case .ended:
+            //scale the newlyCreatedFace to 1x
+            UIView.animate(withDuration: 0.5, animations: {
+                let scaleTransform = CGAffineTransform.init(scaleX: 1, y: 1)
+                self.newlyCreatedFace.transform = scaleTransform
+            })
             break
         default:
             break
@@ -105,10 +142,91 @@ class ViewController: UIViewController {
         sender.setTranslation(CGPoint.zero, in: self.view)
 
     }
+    
+    func onIndividualEmoticonPan(_ sender:UIPanGestureRecognizer ) {
         
-    func moveTray(position:TrayState) {
+        var originalPostion:CGPoint = CGPoint.zero
+        switch sender.state {
+        case .began:
+            originalPostion = (sender.view?.center)!
+            break
+        case .changed:
+            if let view = sender.view {
+                let translation = sender.translation(in: sender.view)
+                view.center = CGPoint(x: view.center.x + translation.x, y: view.center.y+translation.y)
+            }
+            break
+        case .ended:
+            
+            //check if emoticon intersects with the tray view
+//            if let view = sender.view {
+//                if view.frame.intersects(self.trayView.frame) {
+//                    view.center = originalPostion
+//                }
+//            }
+            break
+        default:
+            break
+        }
+        sender.setTranslation(CGPoint.zero, in: self.view)
+        
+    }
+    
+    func onIndividualEmoticonPinch(_ sender:UIPinchGestureRecognizer ) {
+        
+        switch sender.state {
+        case .began:
+            break
+        case .changed:
+            if let view = sender.view {
+                view.transform = view.transform.scaledBy(x: sender.scale, y: sender.scale)
+                sender.scale = 1
+            }
+            
+            break
+        case .ended:
+            
+            break
+        default:
+            break
+        }
+    }
+    
+    func onIndividualEmoticonRotate(_ sender:UIRotationGestureRecognizer) {
+        
+        switch sender.state {
+        case .began:
+            break
+        case .changed:
+            if let view = sender.view {
+                view.transform = view.transform.rotated(by: sender.rotation)
+                sender.rotation = 0
+            }
+            break
+        case .ended:
+            
+            break
+        default:
+            break
+        }
+
+    }
+    
+    func onEmoticonTap(_ sender:UITapGestureRecognizer) {
+        if let view = sender.view {
+            UIView.animate(withDuration: 0.5, animations: {
+                view.transform = CGAffineTransform(scaleX: 0.1, y: 0.1)
+            }, completion: { _ in
+                view.removeFromSuperview()
+
+            })
+        }
+    }
+    
+    
+    func moveTray(currentState:TrayState) {
         var trayPosition = CGPoint.zero
-        switch position {
+        switch currentState {
         case .Open:
             trayPosition = trayOpenCenter
             break
@@ -118,14 +236,31 @@ class ViewController: UIViewController {
             
         }
         
+        if previousState != currentState {
+             self.trayArrow.transform =  self.trayArrow.transform.rotated(by: 3.14)
+        }
+        
+        previousState = currentState;
+        
         UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping:
             0.3, initialSpringVelocity: 1.0, options: [], animations: {
-                
+
                 self.trayView.center = trayPosition
                 
+                
         }) { _ in
-            
+           
         }
+    }
+
+}
+
+
+extension ViewController: UIGestureRecognizerDelegate {
+    
+    //Asks the delegate if two gesture recognizers should be allowed to recognize gestures simultaneously.
+    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer, shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
+        return true
     }
 
 }
